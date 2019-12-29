9434450cc60b366b240fbd8e022ad18aa17c9318
iliana-pap <iliana.pap1@gmail.com>
2019-12-29 17:58:26 +0200

    Preprocessing functions

---
+++ Preprocessing_fun.R
@@ -0,0 +1,56 @@
+set.seed(146)
+
+plotData<-function(sce){
+  plotColData(sce, x = "sum", y="detected",colour_by="cluster_id")
+}
+plotdist<-function(sce){
+  par(mfrow=c(2,2), mar=c(5.1, 4.1, 0.1, 0.1))
+  hist(sce$detected, xlab="Detected Genes", 
+       ylab="Number of cells", breaks=20, main="", col="blue")
+}
+plothighexpression<-function(sce){
+  plotHighestExprs(sce, exprs_values = "counts")
+}
+plotVar<-function(vars){
+  plotExplanatoryVariables(vars)
+}
+plotExpressionProfile<-function(sce){
+  plotExpression(sce, rownames(sce)[1:6], x = "cluster_id",colour_by="cluster_id")
+}
+
+#Preprocessing
+prep_steps <- function(sce){
+  # Doublet removal
+  sce <- scDblFinder(sce, samples = sce$sample_id)
+  sce <- sce[, sce$scDblFinder.class != "doublet"]
+  # 1. remove undetected genes
+  sce <- sce[rowSums(counts(sce) > 0) > 0, ]
+  # `scater` QC & filtering
+  per.cell <- perCellQCMetrics(sce, subsets=list(Mito=grep("mt-", rownames(sce))))
+  print(plotData(sce))
+  colData(sce) <- cbind(colData(sce), per.cell)
+  #remove putative low-quality cells that have very few or many detected genes.
+  print(plotdist(sce))
+  total.drop <- isOutlier(sce$total, nmads = 2, type = "both", log = TRUE, batch = sce$sample_id)
+  detected.drop <- isOutlier(sce$detected, nmads=2, type="lower", log=TRUE)
+  subsets_Mt_percent.drop <- isOutlier(sce$subsets_Mt_percent, nmads = 2, type = "higher") &   sce$subsets_Mt_percent > 0.08
+  sce <- sce[, !(total.drop | detected.drop |   subsets_Mt_percent.drop)]
+  data.frame(ByDetectedGenes=sum(detected.drop),ByTotal=sum(total.drop),Bysubsets_Mt_percent=sum(subsets_Mt_percent.drop),Remaining=ncol(sce))
+  # Remove low-abundance/lowly expressed genes
+  print(plothighexpression(sce))
+  sce <- sce[rowSums(counts(sce) > 1) >= 10, ]
+  # Variable-level QC metrics
+  counts <- assay(sce, "counts")
+  libsizes <- colSums(counts)
+  size.factors <- libsizes/mean(libsizes)
+  logcounts(sce) <- log2(t(t(counts)/size.factors) + 1)
+  vars <- getVarianceExplained(sce, 
+                               variables=c("sample_id", "group_id", "cluster_id","sum","detected","percent_top_50","subsets_Mt_percent"))
+  print(plotVar(vars))
+  # compute sum-factors & normalize
+  sce <- computeLibraryFactors(sce)
+  sce <- logNormCounts(sce)
+  print(plotExpressionProfile(sce))
+  
+  return(sce)
+}
