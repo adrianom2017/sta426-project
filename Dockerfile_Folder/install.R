a21b51c8c74fecab023a88b67f284a25d2f1e246
adrianom <adrianom@student.ethz.ch>
2020-01-08 19:11:50 +0100

    Latest Report

---
+++ Dockerfile_Folder/install.R
@@ -0,0 +1,35 @@
+#Adriano Martinelli, adrianom@student.ethz.ch
+#Script which installs packages
+#Inspired by renku lab script
+
+#Install bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+#Update to given version
+if(BiocManager::version() != '3.10'){
+  BiocManager::install(version='3.10', update=TRUE, ask=FALSE, quiet=TRUE)}
+
+#BiocManager::install('remotes', quiet=TRUE)
+#remotes::install_github('HelenaLC/muscat', quiet=FALSE)
+pkgs <- c("SingleCellExperiment",
+          "scater",
+          "ggplot2","gridExtra","grid",
+          "edgeR",
+          "irlba",
+          "countsimQC",
+          "devtools",
+          "tidyverse",
+          "TMB",
+          "cowplot",
+          "scDblFinder",
+          "dplyr",
+          "UpSetR",
+          "uwot",
+          "Rtsne"
+          )
+
+BiocManager::install(pkgs, update=FALSE, ask=FALSE, quiet=TRUE)
+
+#devtools::install_github("adrianom2017/sta426-project/sta426")
+#devtools::install_github("HelenaLC/muscat", ref="dream2", quiet = TRUE)
