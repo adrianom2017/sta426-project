85f3fa7f7374ed833026a5cf9040e6ba8eef5165
adrianom <adrianom@student.ethz.ch>
2020-01-12 14:33:23 +0100

    Corrected Draft

--- Dockerfile_Folder/install.R
+++ Dockerfile_Folder/install.R
@@ -10,27 +10,25 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 if(BiocManager::version() != '3.10'){
   BiocManager::install(version='3.10', update=TRUE, ask=FALSE, quiet=TRUE)}
 
-#BiocManager::install('remotes', quiet=TRUE)
-#remotes::install_github('HelenaLC/muscat', quiet=FALSE)
-# pkgs <- c("SingleCellExperiment",
-#           "scater",
-#           "ggplot2","gridExtra","grid",
-#           "edgeR",
-#           "irlba",
-#           "countsimQC",
-#           "tidyverse",
-#           "TMB",
-#           "cowplot",
-#           "scDblFinder",
-#           "dplyr",
-#           "UpSetR",
-#           "uwot",
-#           "Rtsne",
-#           "remotes")
-
-pkgs = c("png")
+BiocManager::install('remotes', quiet=TRUE)
+remotes::install_github('HelenaLC/muscat', quiet=FALSE)
+pkgs <- c("SingleCellExperiment",
+          "scater",
+          "ggplot2","gridExtra","grid",
+          "edgeR",
+          "irlba",
+          "countsimQC",
+          "tidyverse",
+          "TMB",
+          "cowplot",
+          "scDblFinder",
+          "dplyr",
+          "UpSetR",
+          "uwot",
+          "Rtsne",
+          "remotes")
 
 BiocManager::install(pkgs, update=FALSE, ask=FALSE, quiet=TRUE)
 warnings()
-#remotes::install_github("adrianom2017/sta426-project/sta426")
-#remotes::install_github("HelenaLC/muscat", ref="dream2", quiet = TRUE)
+remotes::install_github("adrianom2017/sta426-project/sta426")
+remotes::install_github("HelenaLC/muscat", ref="dream2", quiet = TRUE)
